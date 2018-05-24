d281c8e21b4cd50b9845297a94823cce9c0e0580
Amit Gaikwad <amitgaikwad795@gmail.com>
2018-05-25 00:49:53 +0530

    add angular universal behiviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(meta: Meta, title: Title){
+    title.setTitle(' Home Page');
+    meta.addTags([
+      { name: 'author',   content: 'home.com'},
+      { name: 'keywords', content: 'angular seo, angular 4 universal, etc'},
+      { name: 'description', content: 'This is my Angular SEO-based App, enjoy it!' }
+    ]);
+  }
 }
